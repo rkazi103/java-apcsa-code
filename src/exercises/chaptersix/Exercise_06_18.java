3ca7645fdfb2a134acd12c5e206dec37aa7e0a74
Rayan Kazi <rayan.quack.you@gmail.com>
2021-10-19 07:54:00 -0700

    finished all chapter 6 exercises

---
+++ src/exercises/chaptersix/Exercise_06_18.java
@@ -0,0 +1,53 @@
+package exercises.chaptersix;
+
+import java.util.Scanner;
+
+public class Exercise_06_18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter in a good password: ");
+        String password = input.nextLine();
+        validatePassword(password);
+        input.close();
+    }
+
+    public static boolean validatePassword(String password) {
+        int numberOfLetters = 0;
+        int numberOfDigits = 0;
+
+        for (int i = 0; i < password.length(); i++) {
+            if (isLetterOrDigit(password.charAt(i)) == 0) {
+                numberOfLetters++;
+            }
+            if (isLetterOrDigit(password.charAt(i)) == 1) {
+                numberOfDigits++;
+            }
+            if (isLetterOrDigit(password.charAt(i)) == -1) {
+                System.out.println("A password must only contain letters and digits");
+                return false;
+            }
+        }
+
+        if (numberOfLetters < 8) {
+            System.out.println("A password must contain at least 8 characters");
+            return false;
+        }
+        if (numberOfDigits < 2) {
+            System.out.println("A password must contain at least 2 digits");
+            return false;
+        }
+
+        System.out.println("Good Password! ");
+        return true;
+    }
+
+    public static int isLetterOrDigit(char ch) {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+            return 0;
+
+        if (ch >= '0' && ch <= '9')
+            return 1;
+
+        return -1;
+    }
+}
