75ad3fdf7d2e15bdcd4ae5e04d9485022de12c03
Rayan Kazi <rayan.quack.you@gmail.com>
2021-10-19 07:40:52 -0700

    finished exercise 17 chapter 6

---
+++ src/exercises/chaptersix/Exercise_06_17.java
@@ -0,0 +1,22 @@
+package exercises.chaptersix;
+
+import java.util.Scanner;
+
+public class Exercise_06_17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of lines for n by n matrix: ");
+        int numberOfLines = input.nextInt();
+        printMatrix(numberOfLines);
+        input.close();
+    }
+
+    public static void printMatrix(int numberOfLines) {
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < numberOfLines; j++) {
+                System.out.print(Math.random() < 0.5 ? "0 " : "1 ");
+            }
+            System.out.println();
+        }
+    }
+}
